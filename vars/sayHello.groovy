ec7fae0911a06f24fed4be15397c05816063c670
sushant bose <sushant.bose@rci.rogers.com>
2019-04-10 15:48:11 -0400

    add commit status

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,7 +4,7 @@ def call(String name = 'human'){
     echo "Hello, ${name}!!"
 }
 
-def commitStatus(boolean status = 'false', String repo='monorepo', String owner = 'bork'){
+def commitStatus(boolean status = 'false', String repo = 'monorepo', String owner = 'bork'){
     echo "status, ${status}"
     echo "repo, ${repo}"
     echo "owner, ${owner}"
