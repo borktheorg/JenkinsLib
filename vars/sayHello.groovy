3b37ff7b31415e00a861ea3a72eb6841b4daab17
sushant bose <sushant.bose@rci.rogers.com>
2019-04-11 13:50:26 -0400

    add functions

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -5,3 +5,15 @@ def call(String name = 'human', String repo = 'monorepo', String owner = 'bork')
     echo "repo, ${repo}"
     echo "owner, ${owner}"
 }
+
+def getRepoURL() {
+    sh "git config --get remote.origin.url > .git/remote-url"
+    println(readFile(".git/remote-url").trim())
+}
+
+def getCommitSha() {
+    sh "git rev-parse HEAD > .git/current-commit"
+    println(readFile(".git/current-commit").trim())
+}
+
+return this
